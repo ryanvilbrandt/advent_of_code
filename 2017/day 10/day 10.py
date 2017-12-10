92a5024179eff287b87870868f6bfb5f5524643e
Ryan Vilbrandt <ryan.v@zonarsystems.com>
2017-12-10 13:41:28 -0800

    Fixed bug with suffix
    Need to use the decimal values of 17, 31, etc. Not the hexadecimal ones.
    Doy.

--- 2017/day 10/day 10.py
+++ 2017/day 10/day 10.py
@@ -17,6 +17,7 @@ def reverse_chunk(mylist, length):
     return list(reversed(mylist[:length])) + mylist[length:]
 
 # def tie_knot(mylist, lengths):
+#     print(mylist)
 #     current_index = 0
 #     skip_size = 0
 #     for n in lengths:
@@ -25,10 +26,9 @@ def reverse_chunk(mylist, length):
 #         mylist = normalize_list(mylist, (n + skip_size) % len(mylist))
 #         print(mylist)
 #         skip_size += 1
-#     current_index -= 1
-#     mylist = normalize_list(mylist, current_index)
+#     mylist = normalize_list(mylist, current_index * -1)
 #     print(mylist)
-#     return mylist, current_index, skip_size
+#     return mylist
 
 
 # r = list(range(5))
@@ -45,17 +45,16 @@ def tie_knot(mylist, lengths, current_index=0, skip_size=0):
         mylist = reverse_chunk(mylist, n)
         current_index = (current_index + n + skip_size) % len(mylist)
         mylist = normalize_list(mylist, (n + skip_size) % len(mylist))
-        print(mylist)
         skip_size += 1
     return mylist, current_index, skip_size
 
-SUFFIX = "\x17\x31\x73\x47\x23"
+SUFFIX = [17, 31, 73, 47, 23]
 
 def calc_hash(myinput, rounds=64, suffix=SUFFIX, list_length=256):
     print(myinput)
     mylist = list(range(list_length))
-    myinput += suffix
     lengths = [ord(c) for c in myinput]
+    lengths += suffix
 
     current_index = 0
     skip_size = 0
@@ -63,7 +62,6 @@ def calc_hash(myinput, rounds=64, suffix=SUFFIX, list_length=256):
         mylist, current_index, skip_size = tie_knot(mylist, lengths, current_index, skip_size)
     # Convert list to original orientation
     mylist = normalize_list(mylist, current_index * -1)
-    print(mylist)
 
     def xor_chunk(chunk):
         x = 0
@@ -74,3 +72,7 @@ def calc_hash(myinput, rounds=64, suffix=SUFFIX, list_length=256):
     return "".join("{:>02x}".format(c) for c in num_hash)
 
 assert calc_hash("") == "a2582a3a0e66e6e86e3812dcb672a272"
+assert calc_hash("AoC 2017") == "33efeb34ea91902bb2f59c9920caa6cd"
+assert calc_hash("1,2,3") == "3efbe78a8d82f29979031a4aa0b16a9d"
+assert calc_hash("1,2,4") == "63960835bcdc130f0b66d7ff4f6a5a8e"
+print(calc_hash("227,169,3,166,246,201,0,47,1,255,2,254,96,3,97,144"))
