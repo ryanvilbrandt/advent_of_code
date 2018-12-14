e69c7353eb395b1d571787254db69168045b3b63
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2018-12-14 12:59:24 +0100

    Solved Day 5 Part B

--- 2018/day 5/day 5.py
+++ 2018/day 5/day 5.py
@@ -7,12 +7,12 @@ day_5_test_2 = "BbAAaBb"
 reg = re.compile(r"(?=([a-z])(\1))", re.IGNORECASE)
 
 
-def collapse_polymer(polymer):
-    iters = 0
+def react_polymer(polymer):
+    # iters = 0
     while True:
-        iters += 1
-        if iters % 1000 == 0:
-            print(f"{iters}: {len(polymer)}")
+        # iters += 1
+        # if iters % 1000 == 0:
+        #     print(f"{iters}: {len(polymer)}")
         matches = re.findall(reg, polymer)
         for m in matches:
             if m[0] != m[1]:
@@ -23,8 +23,24 @@ def collapse_polymer(polymer):
             return polymer
 
 
-assert len(collapse_polymer(day_5_example)) == 10
-assert len(collapse_polymer(day_5_test_1)) == 3
-assert len(collapse_polymer(day_5_test_2)) == 1
+assert len(react_polymer(day_5_example)) == 10
+assert len(react_polymer(day_5_test_1)) == 3
+assert len(react_polymer(day_5_test_2)) == 1
 with open("day 5.input") as f:
-    print(len(collapse_polymer(f.read().strip())))
+    print(len(react_polymer(f.read().strip())))
+
+
+def find_shortest_polymer(polymer: str):
+    polymer_set = sorted(list(set(polymer.lower())))
+    polymer_dict = {}
+    for p in polymer_set:
+        polymer_candidate = polymer.replace(p, '').replace(p.upper(), '')
+        reacted_polymer = react_polymer(polymer_candidate)
+        print(f"{p} => {len(reacted_polymer)}")
+        polymer_dict[p] = len(reacted_polymer)
+    return min(polymer_dict.values())
+
+
+assert find_shortest_polymer(day_5_example) == 4
+with open("day 5.input") as f:
+    print(find_shortest_polymer(f.read().strip()))
