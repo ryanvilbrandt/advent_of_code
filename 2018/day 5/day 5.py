3bd2588faf41dd74a84c72d03144de0ef2fcfc1d
Ryan Vilbrandt <ryan.vilbrandt@gmail.com>
2018-12-14 12:46:17 +0100

    Solved day 5 part A

--- 2018/day 5/day 5.py
+++ 2018/day 5/day 5.py
@@ -1,8 +1,10 @@
 import re
 
 day_5_example = "dabAcCaCBAcCcaDA"
+day_5_test_1 = "bbAAaBb"
+day_5_test_2 = "BbAAaBb"
 
-reg = re.compile(r"([a-z])(\1)", re.IGNORECASE)
+reg = re.compile(r"(?=([a-z])(\1))", re.IGNORECASE)
 
 
 def collapse_polymer(polymer):
@@ -15,12 +17,14 @@ def collapse_polymer(polymer):
         for m in matches:
             if m[0] != m[1]:
                 # print(polymer)
-                polymer = polymer.replace(''.join(m), '', 1)
+                polymer = polymer.replace(''.join(m), '')
                 break
         else:
             return polymer
 
 
-print(len(collapse_polymer(day_5_example)))
+assert len(collapse_polymer(day_5_example)) == 10
+assert len(collapse_polymer(day_5_test_1)) == 3
+assert len(collapse_polymer(day_5_test_2)) == 1
 with open("day 5.input") as f:
-    print(len(collapse_polymer(f.read())))
+    print(len(collapse_polymer(f.read().strip())))
