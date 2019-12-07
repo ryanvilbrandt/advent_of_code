298cd0846da924f8f40dfea609efd1d5cb3c3c26
ryanvilbrandt <ryan.v@zonarsystems.com>
2019-12-07 11:59:07 -0800

    Added part 1 solution

--- aoc_2019/day_7/test_problem.py
+++ aoc_2019/day_7/test_problem.py
@@ -22,6 +22,20 @@ class TestDay7(unittest.TestCase):
     def test_example_program_1(self):
         self.assertEqual(43210, find_max_thruster_setting("3,15,3,16,1002,16,10,16,1,16,15,15,4,15,99,0,0"))
 
+    def test_example_program_2(self):
+        self.assertEqual(54321, find_max_thruster_setting(
+            "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"
+        ))
+
+    def test_example_program_3(self):
+        self.assertEqual(65210, find_max_thruster_setting(
+            "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
+        ))
+
+    def test_part_1(self):
+        with open("day_7/input.text") as f:
+            self.assertEqual(14902, find_max_thruster_setting(f.read()))
+
 
 if __name__ == "__main__":
     unittest.main()
