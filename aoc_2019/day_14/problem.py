e7a9a2089561fc5194f9921a0a864e1c4332fb91
ryanvilbrandt <ryan.v@zonarsystems.com>
2019-12-14 17:41:02 -0800

    Finished Day 14

--- aoc_2019/day_14/problem.py
+++ aoc_2019/day_14/problem.py
@@ -1,4 +1,5 @@
 import re
+from collections import defaultdict
 from math import ceil
 from typing import Dict
 
@@ -6,12 +7,12 @@ from typing import Dict
 class Reaction:
 
     product = None
-    amount = None
+    produced_amount = None
     ingredients = None
 
     def __init__(self, product: str, amount: int, ingredients: Dict[str, int]):
         self.product = product
-        self.amount = amount
+        self.produced_amount = amount
         self.ingredients = ingredients
 
     def get_ingredients(self) -> Dict[str, int]:
@@ -34,15 +35,19 @@ class Reaction:
         return repr(self)
 
     def __repr__(self):
-        return f"Reaction({self.product}, {self.amount}, {self.ingredients})"
+        return f"Reaction({self.product!r}, {self.produced_amount!r}, {self.ingredients})"
 
 
 class ReactionTree:
 
     reactions = None
+    inventory = None
+    ore_used = None
 
     def __init__(self):
         self.reactions = {}
+        self.inventory = defaultdict(int)
+        self.ore_used = 0
 
     def add_reaction(self, reaction: Reaction):
         self.reactions[reaction.product] = reaction
@@ -61,14 +66,37 @@ class ReactionTree:
     def get_reaction(self, name: str) -> Reaction:
         return self.reactions[name]
 
-    def get_ore_count_per_fuel(self):
-        return self.get_ore_count("FUEL")
+    def get_ore_count_per_fuel(self, fuel_amount=1):
+        self.inventory = defaultdict(int)
+        self.ore_used = 0
+        self.make_item("FUEL", fuel_amount)
+        return self.ore_used
 
-    def get_ore_count(self, name: str):
+    def make_item(self, name: str, requested_amount: int):
         if name == "ORE":
-            return 1
-        node = self.get_reaction(name)
-        total_ore_count = 0
-        for ing, ing_amount in node.get_ingredients().items():
-            total_ore_count += ceil((ing_amount * self.get_ore_count(ing)) / node.amount)
-        return total_ore_count
+            self.ore_used += requested_amount
+            return
+        if requested_amount > self.inventory[name]:
+            node = self.get_reaction(name)
+            quantity = int(ceil((requested_amount - self.inventory[name]) / node.produced_amount))
+            for ing, ing_amount in node.get_ingredients().items():
+                self.make_item(ing, ing_amount * quantity)
+            self.inventory[name] += node.produced_amount * quantity
+        self.inventory[name] -= requested_amount
+        assert self.inventory[name] >= 0
+
+    def find_fuel_output(self, ore_provided: int, fuel_amount_to_check=1):
+        last_fuel_amount = fuel_amount_to_check
+        while True:
+            ore_count = self.get_ore_count_per_fuel(fuel_amount_to_check)
+            print("Ore count:", ore_count)
+            error_ratio = ore_provided / ore_count
+            print("Error ratio:", error_ratio)
+            fuel_amount_to_check = int(error_ratio * fuel_amount_to_check)
+            print("Fuel amount to check:", fuel_amount_to_check)
+            if last_fuel_amount == fuel_amount_to_check:
+                break
+            last_fuel_amount = fuel_amount_to_check
+
+        return fuel_amount_to_check
+
--- aoc_2019/day_14/test_problem.py
+++ aoc_2019/day_14/test_problem.py
@@ -7,7 +7,7 @@ class TestDay14(unittest.TestCase):
 
     def test_Reaction_from_string(self):
         reaction = Reaction.from_string("7 A, 1 E => 1 FUEL")
-        print(reaction)
+        self.assertEqual(str(Reaction('FUEL', 1, {'A': 7, 'E': 1})), str(reaction))
 
     def test_ReactionTree_from_string(self):
         s = """
@@ -19,7 +19,16 @@ class TestDay14(unittest.TestCase):
             7 A, 1 E => 1 FUEL
         """
         reaction_tree = ReactionTree.from_string(s)
-        reaction_tree.print_reactions()
+        expected = {
+            'A': Reaction('A', 10, {'ORE': 10}),
+            'B': Reaction('B', 1, {'ORE': 1}),
+            'C': Reaction('C', 1, {'A': 7, 'B': 1}),
+            'D': Reaction('D', 1, {'A': 7, 'C': 1}),
+            'E': Reaction('E', 1, {'A': 7, 'D': 1}),
+            'FUEL': Reaction('FUEL', 1, {'A': 7, 'E': 1})
+        }
+        actual = reaction_tree.reactions
+        self.assertEqual(str(expected), str(actual))
 
     def test_get_ore_count_per_fuel_1(self):
         self.assertEqual(1, ReactionTree.from_string("1 ORE => 1 FUEL").get_ore_count_per_fuel())
@@ -37,7 +46,15 @@ class TestDay14(unittest.TestCase):
         s = """
             4 ORE => 2 FUEL
         """
-        self.assertEqual(2, ReactionTree.from_string(s).get_ore_count_per_fuel())
+        self.assertEqual(4, ReactionTree.from_string(s).get_ore_count_per_fuel())
+        s = """
+            4 ORE => 3 FUEL
+        """
+        self.assertEqual(4, ReactionTree.from_string(s).get_ore_count_per_fuel())
+        s = """
+            3 ORE => 2 FUEL
+        """
+        self.assertEqual(3, ReactionTree.from_string(s).get_ore_count_per_fuel())
         s = """
             4 ORE => 2 A
             2 A => 1 FUEL
@@ -48,6 +65,143 @@ class TestDay14(unittest.TestCase):
             3 A => 1 FUEL
         """
         self.assertEqual(8, ReactionTree.from_string(s).get_ore_count_per_fuel())
+
+    def test_day_14_example_1(self):
+        s = """
+            10 ORE => 10 A
+            1 ORE => 1 B
+            7 A, 1 B => 1 C
+            7 A, 1 C => 1 D
+            7 A, 1 D => 1 E
+            7 A, 1 E => 1 FUEL
+        """
+        self.assertEqual(31, ReactionTree.from_string(s).get_ore_count_per_fuel())
+
+    def test_day_14_example_2(self):
+        s = """
+            9 ORE => 2 A
+            8 ORE => 3 B
+            7 ORE => 5 C
+            3 A, 4 B => 1 AB
+            5 B, 7 C => 1 BC
+            4 C, 1 A => 1 CA
+            2 AB, 3 BC, 4 CA => 1 FUEL
+        """
+        self.assertEqual(165, ReactionTree.from_string(s).get_ore_count_per_fuel())
+
+    def test_day_14_example_3(self):
+        s = """
+            157 ORE => 5 NZVS
+            165 ORE => 6 DCFZ
+            44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+            12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+            179 ORE => 7 PSHF
+            177 ORE => 5 HKGWZ
+            7 DCFZ, 7 PSHF => 2 XJWVT
+            165 ORE => 2 GPVTF
+            3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT
+        """
+        self.assertEqual(13312, ReactionTree.from_string(s).get_ore_count_per_fuel())
+
+    def test_day_14_example_4(self):
+        s = """
+            2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+            17 NVRVD, 3 JNWZP => 8 VPVL
+            53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+            22 VJHF, 37 MNCFX => 5 FWMGM
+            139 ORE => 4 NVRVD
+            144 ORE => 7 JNWZP
+            5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+            5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+            145 ORE => 6 MNCFX
+            1 NVRVD => 8 CXFTF
+            1 VJHF, 6 MNCFX => 4 RFSQX
+            176 ORE => 6 VJHF
+        """
+        self.assertEqual(180697, ReactionTree.from_string(s).get_ore_count_per_fuel())
+
+    def test_day_14_example_5(self):
+        s = """
+            171 ORE => 8 CNZTR
+            7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+            114 ORE => 4 BHXH
+            14 VRPVC => 6 BMBT
+            6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+            6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+            15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+            13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+            5 BMBT => 4 WPTQ
+            189 ORE => 9 KTJDG
+            1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+            12 VRPVC, 27 CNZTR => 2 XDBXC
+            15 KTJDG, 12 BHXH => 5 XCVML
+            3 BHXH, 2 VRPVC => 7 MZWV
+            121 ORE => 7 VRPVC
+            7 XCVML => 6 RJRHP
+            5 BHXH, 4 VRPVC => 5 LTCX
+        """
+        self.assertEqual(2210736, ReactionTree.from_string(s).get_ore_count_per_fuel())
+
+    def test_day_14_part_1(self):
+        with open("input.text") as f:
+            self.assertEqual(857266, ReactionTree.from_string(f.read()).get_ore_count_per_fuel())
+
+    def test_day_14_example_6(self):
+        s = """
+            157 ORE => 5 NZVS
+            165 ORE => 6 DCFZ
+            44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+            12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+            179 ORE => 7 PSHF
+            177 ORE => 5 HKGWZ
+            7 DCFZ, 7 PSHF => 2 XJWVT
+            165 ORE => 2 GPVTF
+            3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT
+        """
+        self.assertEqual(82892753, ReactionTree.from_string(s).find_fuel_output(1e12))
+
+    def test_day_14_example_7(self):
+        s = """
+            2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+            17 NVRVD, 3 JNWZP => 8 VPVL
+            53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+            22 VJHF, 37 MNCFX => 5 FWMGM
+            139 ORE => 4 NVRVD
+            144 ORE => 7 JNWZP
+            5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+            5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+            145 ORE => 6 MNCFX
+            1 NVRVD => 8 CXFTF
+            1 VJHF, 6 MNCFX => 4 RFSQX
+            176 ORE => 6 VJHF
+        """
+        self.assertEqual(5586022, ReactionTree.from_string(s).find_fuel_output(1e12))
+
+    def test_day_14_example_8(self):
+        s = """
+            171 ORE => 8 CNZTR
+            7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+            114 ORE => 4 BHXH
+            14 VRPVC => 6 BMBT
+            6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+            6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+            15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+            13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+            5 BMBT => 4 WPTQ
+            189 ORE => 9 KTJDG
+            1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+            12 VRPVC, 27 CNZTR => 2 XDBXC
+            15 KTJDG, 12 BHXH => 5 XCVML
+            3 BHXH, 2 VRPVC => 7 MZWV
+            121 ORE => 7 VRPVC
+            7 XCVML => 6 RJRHP
+            5 BHXH, 4 VRPVC => 5 LTCX
+        """
+        self.assertEqual(460664, ReactionTree.from_string(s).find_fuel_output(1e12))
+
+    def test_day_14_part_2(self):
+        with open("input.text") as f:
+            self.assertEqual(2144702, ReactionTree.from_string(f.read()).find_fuel_output(1e12))
 
 
 if __name__ == "__main__":
