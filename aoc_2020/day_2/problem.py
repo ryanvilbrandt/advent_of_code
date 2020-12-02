069c2de78ab035a554581b013cded79f694e6f0f
ryanvilbrandt <ryan.v@zonarsystems.com>
2020-12-02 06:54:49 -0800

    Update problem.py

--- aoc_2020/day_2/problem.py
+++ aoc_2020/day_2/problem.py
@@ -3,11 +3,15 @@ import re
 from aoc_2020.common import NoRegexMatch, text_to_list
 
 
-def check_password_part_1(line):
+def parse_line(line):
     m = re.match(r"(\d+)-(\d+) (.*?): (.*)", line)
     if not m:
         raise NoRegexMatch(line)
-    minimum, maximum, character, password = int(m.group(1)), int(m.group(2)), m.group(3), m.group(4)
+    return int(m.group(1)), int(m.group(2)), m.group(3), m.group(4)
+
+
+def check_password_part_1(line):
+    minimum, maximum, character, password = parse_line(line)
     character_count = password.count(character)
     return minimum <= character_count <= maximum
 
@@ -19,10 +23,7 @@ def check_passwords_part_1(text):
 
 
 def check_password_part_2(line):
-    m = re.match(r"(\d+)-(\d+) (.*?): (.*)", line)
-    if not m:
-        raise NoRegexMatch(line)
-    pos_1, pos_2, character, password = int(m.group(1)), int(m.group(2)), m.group(3), m.group(4)
+    pos_1, pos_2, character, password = parse_line(line)
     a = password[pos_1 - 1]
     b = password[pos_2 - 1]
     return (a == character) ^ (b == character)
