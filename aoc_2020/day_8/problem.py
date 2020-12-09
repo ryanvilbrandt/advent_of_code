c0e521c55902b2dad975361f85c88c4e923e2e9d
ryanvilbrandt <ryan.v@zonarsystems.com>
2020-12-08 16:21:10 -0800

    Updated to NamedTuple

--- aoc_2020/day_8/problem.py
+++ aoc_2020/day_8/problem.py
@@ -1,7 +1,12 @@
 import re
-from typing import Tuple, List
+from typing import Tuple, List, NamedTuple
 
 from aoc_2020.common import text_to_list
+
+
+class Instruction(NamedTuple):
+    op: str
+    val: int
 
 
 class InfiniteLoopError(Exception):
@@ -11,35 +16,35 @@ class InfiniteLoopError(Exception):
 accumulator = 0
 
 
-def perform_instruction(instruction: Tuple[str, int]) -> int:
+def perform_instruction(instruction: Instruction) -> int:
     global accumulator
-    if instruction[0] == "jmp":
-        return instruction[1]
-    if instruction[0] == "nop":
+    if instruction.op == "jmp":
+        return instruction.val
+    if instruction.op == "nop":
         pass
-    elif instruction[0] == "acc":
-        accumulator += instruction[1]
+    elif instruction.op == "acc":
+        accumulator += instruction.val
     return 1
 
 
-def run_program(instructions_list: List[Tuple[str, int]]) -> int:
+def run_program(instructions_list: List[Instruction]) -> int:
     global accumulator
-    past_ptrs = []
+    past_ptrs = set()
     ptr = 0
     accumulator = 0
     while ptr < len(instructions_list):
-        past_ptrs.append(ptr)
+        past_ptrs.add(ptr)
         ptr += perform_instruction(instructions_list[ptr])
         if ptr in past_ptrs:
             raise InfiniteLoopError(accumulator)
     return accumulator
 
 
-def load_program(instructions_text: str) -> List[Tuple[str, int]]:
+def load_program(instructions_text: str) -> List[Instruction]:
     instructions_list = []
     for instruction in text_to_list(instructions_text):
         m = re.search(r"([a-z]{3}) ([+\-]\d+)", instruction)
-        instructions_list.append((m.group(1), int(m.group(2))))
+        instructions_list.append(Instruction(m.group(1), int(m.group(2))))
     return instructions_list
 
 
@@ -51,14 +56,14 @@ def load_and_run(instructions_text: str) -> int:
 def fix_program(instructions_text: str):
     instructions_list = load_program(instructions_text)
     for i, x in enumerate(instructions_list):
-        if x[0] == "nop":
+        if x.op == "nop":
             new_instruction = "jmp"
-        elif x[0] == "jmp":
+        elif x.op == "jmp":
             new_instruction = "nop"
         else:
             continue
         instructions_list_copy = instructions_list[:]
-        instructions_list_copy[i] = (new_instruction, x[1])
+        instructions_list_copy[i] = Instruction(new_instruction, x.val)
         try:
             acc = run_program(instructions_list_copy)
             return acc
